101df8e51cd74e6deee0800442c3518b53e5e198
4sadmansakib4 <iamsadman0@gmail.com>
2022-11-09 17:31:12 +0600

    sadman

--- index.php
+++ index.php
@@ -1,100 +1,65 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Login Form</title>
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-      <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    
-    <body>
-
 <?php
- $emailmsg="";
- $pasdmsg="";
- $msg="";
 
- $ademailmsg="";
- $adpasdmsg="";
+include 'database_connection.php';
+include 'function.php';
 
-
- if(!empty($_REQUEST['ademailmsg'])){
-    $ademailmsg=$_REQUEST['ademailmsg'];
- }
-
- if(!empty($_REQUEST['adpasdmsg'])){
-    $adpasdmsg=$_REQUEST['adpasdmsg'];
- }
-
- if(!empty($_REQUEST['emailmsg'])){
-    $emailmsg=$_REQUEST['emailmsg'];
- }
-
- if(!empty($_REQUEST['pasdmsg'])){
-  $pasdmsg=$_REQUEST['pasdmsg'];
+if(is_user_login())
+{
+	header('location:issue_book_details.php');
 }
 
-if(!empty($_REQUEST['msg'])){
-    $msg=$_REQUEST['msg'];
- }
-
- ?>
+include 'header.php';
 
 
 
-<div class="container login-container">
-<div class="row"><h4><?php echo $msg?></h4></div>
-            <div class="row">
-                <div class="col-md-6 login-form-1">
-                    <h3>Student Login</h3>
-                    <form action="login_server_page.php" method="get">
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="login_email" placeholder="Your Email *" value="" />
-                        </div>
-                        <Label style="color:red">*<?php echo $emailmsg?></label>
-                        <div class="form-group">
-                            <input type="password" class="form-control" name="login_pasword"  placeholder="Your Password *" value="" />
-                        </div>
-                        <Label style="color:red">*<?php echo $pasdmsg?></label>
-                        <div class="form-group">
-                            <input type="submit" class="btnSubmit" value="Login" />
-                        </div>
-                    </form>
-                </div>
-                <div class="col-md-6 login-form-2">
-                    <h3>Admin Login</h3>
-                    <form action="loginadmin_server_page.php" method="get">
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="login_email" placeholder="Your Email *" value="" />
-                        </div>
-                        <Label style="color:red">*<?php echo $ademailmsg?></label>
-                        <div class="form-group">
-                            <input type="password" class="form-control" name="login_pasword"  placeholder="Your Password *" value="" />
-                        </div>
-                        <Label style="color:red">*<?php echo $adpasdmsg?></label>
-                        <div class="form-group">
-                            <input type="submit" class="btnSubmit" value="Login" />
-                        </div>
-                        <div class="form-group">
+?>
 
-                            <a href="#" class="ForgetPwd" value="Login">Forget Password?</a>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+<div class="p-5 mb-4 bg-light rounded-3">
 
+	<div class="container-fluid py-5">
 
+		<h1 class="display-5 fw-bold">Library Management System</h1>
 
-        
+		<p class="fs-4">This is simple Library Management System which use for maintain the record of the library. This Library Managment System has been made by using PHP script, MySQL Database, Vanilla JavaScript and Bootstrap 5 framework. This is PHP Project on Online Library Management System.</p>
 
+	</div>
 
+</div>
 
-        <script src="" async defer></script>
-    </body>
-</html>
+<div class="row align-items-md-stretch">
+
+	<div class="col-md-6">
+
+		<div class="h-100 p-5 text-white bg-dark rounded-3">
+
+			<h2>Admin Login</h2>
+			<p></p>
+			<a href="admin_login.php" class="btn btn-outline-light">Admin Login</a>
+
+		</div>
+
+	</div>
+
+	<div class="col-md-6">
+
+		<div class="h-100 p-5 bg-light border rounded-3">
+
+			<h2>User Login</h2>
+
+			<p></p>
+
+			<a href="user_login.php" class="btn btn-outline-secondary">User Login</a>
+
+			<a href="user_registration.php" class="btn btn-outline-primary">User Sign Up</a>
+
+		</div>
+
+	</div>
+
+</div>
+
+<?php
+
+include 'footer.php';
+
+?>
